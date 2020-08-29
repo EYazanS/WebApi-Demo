fae51f5f07ece313afcdff2f8ace017ab68da072
Yazan Salameh <e.yazan@live.com>
2020-08-29 11:48:47 +0300

    Added validation

--- Business/Resources/PersonResource.cs
+++ Business/Resources/PersonResource.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Business.Resources
 {
     public class PersonResource : BaseResource
     {
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Name length should be between {1} and {0}"), Required]
         public string FullName { get; set; }
 
+        [Required]
         public DateTime DateOfBirth { get; set; }
     }
 }
